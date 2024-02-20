44da18f2c135fe92dbef4e64d6036d42b171a57c
Mank <2995475603@qq.com>
2024-02-20 16:52:02 +0800

    add HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    // String args[] 与 String[] args 都可以执行，但推荐使用 String[] args，这样可以避免歧义和误读。
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
